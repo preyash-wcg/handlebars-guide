4240112c6a5699c7480696dba671b60f2618a82b
Preyash <preyash.p@webcodegenie.com>
2024-06-07 14:46:49 +0530

    code format

--- index.js
+++ index.js
@@ -1,45 +1,52 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-const handlebars = require('express-handlebars');
-app.set('view engine', 'hbs');
-app.engine('hbs', handlebars.engine({
-    layoutsDir: __dirname + '/views/layouts',
-    extname: 'hbs',
-    defaultLayout: 'planB',
-    partialsDir: __dirname + '/views/partials/'
-}));
+const handlebars = require("express-handlebars");
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  handlebars.engine({
+    layoutsDir: __dirname + "/views/layouts",
+    extname: "hbs",
+    defaultLayout: "planB",
+    partialsDir: __dirname + "/views/partials/",
+  })
+);
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 fakeApi = () => {
-return [
-        {
-            name: 'Katarina',
-            lane: 'midlaner'
-        },
-        {
-            name: 'Jayce',
-            lane: 'toplaner'
-        },
-        {
-            name: 'Heimerdinger',
-            lane: 'toplaner'
-        },
-        {
-            name: 'Zed',
-            lane: 'midlaner'
-        },
-        {
-            name: 'Azir',
-            lane: 'midlaner'
-        }
-];
-}
+  return [
+    {
+      name: "Katarina",
+      lane: "midlaner",
+    },
+    {
+      name: "Jayce",
+      lane: "toplaner",
+    },
+    {
+      name: "Heimerdinger",
+      lane: "toplaner",
+    },
+    {
+      name: "Zed",
+      lane: "midlaner",
+    },
+    {
+      name: "Azir",
+      lane: "midlaner",
+    },
+  ];
+};
 
-app.get('/', (req, res) => {
-    res.render('main', {layout: 'index', suggestedChamps: fakeApi(), listExists: true});
+app.get("/", (req, res) => {
+  res.render("main", {
+    layout: "index",
+    suggestedChamps: fakeApi(),
+    listExists: true,
+  });
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
